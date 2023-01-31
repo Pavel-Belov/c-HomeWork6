78f57f4e17f1c597d28fdfa54eb10e3999c0ddc6
Pavel Belov <job.pb@yandex.ru>
2023-01-31 23:03:03 +0300

    fix: fix code

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -27,8 +27,6 @@ void FillArray(int[,] array)
 
 void ChangeArray(int[,] array)
 {
-    Random random = new Random();
-
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
