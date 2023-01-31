cf64d2c4181219753ba373c95768f8f2a3128bf8
Pavel Belov <job.pb@yandex.ru>
2023-01-31 22:58:36 +0300

    add home work 6 task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,54 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(10);
+        }
+    }
+}
+
+void ChangeArray(int[,] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                array[i, j] *= array[i, j];
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк двумерного массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов двумерного массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[rows, columns];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
