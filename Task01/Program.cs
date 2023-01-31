ed91da581f1b20d483e275218cbe9bb4b1909982
Pavel Belov <job.pb@yandex.ru>
2023-01-31 22:48:05 +0300

    add home work 6 task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,37 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(random.Next(10) + random.NextDouble(), 3);
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк двумерного массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов двумерного массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = new double[rows, columns];
+
+FillArray(array);
+PrintArray(array);
