05ca5c6d3e636f2ea9bea04a3d99d8dba8cab16d
Pavel Belov <job.pb@yandex.ru>
2023-01-31 22:51:49 +0300

    add home work 6 task 02

---
+++ Task02/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк двумерного массива m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов двумерного массива n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
